a7a09f397a4405a99491eba8c58c4edb8bfbf0e0
Lai <temoa_yui@163.com>
2023-11-25 17:49:30 +0800

    config iOS

--- ios/Classes/BuglyFlutterPlugin.swift
+++ ios/Classes/BuglyFlutterPlugin.swift
@@ -1,17 +1,57 @@
 import Flutter
 import UIKit
+import Bugly
 
 public class BuglyFlutterPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "bugly_flutter", binaryMessenger: registrar.messenger())
+    let channel = FlutterMethodChannel(name: "temoa/bugly_flutter", binaryMessenger: registrar.messenger())
     let instance = BuglyFlutterPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
-
+  
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
+    case "initCrashReport":
+      let arguments = call.arguments as! Dictionary<String, Any>
+      let iOSAppId = arguments["iOSAppId"] as! String
+      let debug = arguments["debug"] as! Bool
+      
+      let deviceId = arguments["deviceId"] as? String
+      let appChannel = arguments["appChannel"] as? String
+      let appVersion = arguments["appVersion"]as? String
+      let isDevelopmentDevice = arguments["isDevelopmentDevice"] as! Bool
+      
+      let config = BuglyConfig()
+      if(deviceId != nil){
+        config.deviceIdentifier = deviceId!
+      }
+      if(appChannel != nil){
+        config.channel = appChannel!
+      }
+      if(appVersion != nil){
+        config.version = appVersion!
+      }
+      if(appVersion != nil){
+        config.version = appVersion!
+      }
+      
+      Bugly.start(withAppId: iOSAppId, developmentDevice: isDevelopmentDevice, config: config)
+      result(nil)
+      break;
+    case "postCaughtException":
+      let arguments = call.arguments as! Dictionary<String, Any>
+      let crashMessage = arguments["crashMessage"] as! String
+      
+      let crashDetail = arguments["crashDetail"] as! String
+      let stackTraceArray = crashDetail.components(separatedBy: "")
+      
+      var data = arguments["crashData"] as? [AnyHashable : Any]
+      if data == nil {
+          data = [:]
+      }
+      Bugly.reportException(withCategory: 5, name: "Flutter Exception", reason: crashMessage, callStack: stackTraceArray, extraInfo: data!, terminateApp:false)
+      result(nil)
+      break;
     default:
       result(FlutterMethodNotImplemented)
     }
--- ios/bugly_flutter.podspec
+++ ios/bugly_flutter.podspec
@@ -10,14 +10,26 @@ Pod::Spec.new do |s|
 Bugly Flutter SDK
                        DESC
   s.homepage         = 'http://example.com'
-  s.license          = { :file => '../LICENSE' }
-  s.author           = { 'Your Company' => 'email@example.com' }
-  s.source           = { :path => '.' }
-  s.source_files = 'Classes/**/*'
-  s.dependency 'Flutter'
-  s.platform = :ios, '11.0'
+s.
+license = {:file => '../LICENSE'}
+s.
+author = {'Your Company' => 'email@example.com'}
+s.
+source = {:path => '.'}
+s.
+source_files = 'Classes/**/*'
+s.dependency 'Flutter'
+s.
+platform =
+:ios, '11.0'
 
-  # Flutter.framework does not contain a i386 slice.
-  s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES', 'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'i386' }
-  s.swift_version = '5.0'
+# Flutter.framework does not contain a i386 slice.
+s.
+pod_target_xcconfig = {'DEFINES_MODULE' => 'YES', 'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'i386'}
+s.
+swift_version = '5.0'
+
+s.
+static_framework = true
+s.dependency 'Bugly'
 end
